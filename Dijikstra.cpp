66d527dd14b1d5c81ae721ca18fa67c3ee264498
surajpatil4899 <85224768+surajpatil4899@users.noreply.github.com>
2022-07-19 14:25:54 +0530

    Add files via upload

---
+++ Dijikstra.cpp
@@ -0,0 +1,31 @@
+  int dijkstra(int source,int n,vector<pair<int,int>> g[n+1]){
+        vector<int> vis(n+1, 0);
+        vector<int> dist(n+1, INF);
+        
+        set<pair<int,int>> st;
+        st.insert({0, source});
+        dist[source] = 0;
+        while(st.size()>0){
+            auto node = *st.begin();
+            int v = node.second;
+            int v_dist = node.first;
+            st.erase(st.begin());
+            if(vis[v]) continue;
+            vis[v] = 1;
+            for(auto child: g[v]){
+                int child_v = child.first;
+                int wt = child.second;
+                if(dist[v] + wt < dist[child_v]){
+                    dist[child_v] = dist[v] + wt;
+                    st.insert({dist[child_v], child_v}); 
+                }
+            }
+        }
+        int ans = 0;
+        for(int i=1;i<=n;i++){
+            if(dist[i]==INF) return -1;
+            ans = max(ans, dist[i]);
+        }
+        
+        return ans;
+    }
